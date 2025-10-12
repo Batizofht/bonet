0c5288264ca645073197ec16f21f41ac6c717e1e
batizofht <vlabatizo@gmail.com>
2025-10-12 10:32:18 +0200

    added the top progresss

--- i18n.ts
+++ i18n.ts
@@ -1,21 +1,23 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import en from './public/locales/en/translation.json';
+import fr from './public/locales/fr/translation.json';
+import ch from "./public/locales/ch/translation.json"
 
 i18n
-  .use(HttpApi) // load translations via HTTP
-  .use(LanguageDetector) // detect user language
-  .use(initReactI18next) // pass the i18n instance to react-i18next
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
-    fallbackLng: 'en',  // default language fallback
-    debug: true,        // turn off in production
-    interpolation: {
-      escapeValue: false, // react already safe from xss
+    resources: {
+      en: { translation: en },
+      fr: { translation: fr },
+      ch: { translation: ch },
     },
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json', // path to translations files
-    },
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: { escapeValue: false },
+    react: { useSuspense: false }, // no suspense needed since data is already available
   });
 
 export default i18n;
--- src/app/LayoutWrapper.tsx
+++ src/app/LayoutWrapper.tsx
@@ -5,8 +5,29 @@ import SuperFooter from "@/components/Superfooter";
 import ChatBot from "@/components/bot";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+NProgress.configure({ showSpinner: false });
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
+   const pathname = usePathname();
+
+  useEffect(() => {
+    NProgress.start(); // start loading bar
+    window.scrollTo(0, 0);
+
+    const timer = setTimeout(() => {
+      NProgress.done(); // end loading bar
+    }, 500); // adjust duration if needed
+
+    return () => {
+      clearTimeout(timer); // cleanup
+      NProgress.done(); // ensure bar ends if unmounted
+    };
+  }, [pathname]);
+
   return (
     <>
       <Navbar />
